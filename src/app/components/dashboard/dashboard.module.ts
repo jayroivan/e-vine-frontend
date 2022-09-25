1ed9d639904d087b18f546f95831261bc0ef85f7
dani3lm3na1999 <dani3lm3na@gmail.com>
2022-09-24 19:37:41 -0600

    creacion de navbar 1.1

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../shared/shared.module';
-import { NabvarComponent } from './nabvar/nabvar.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    NabvarComponent
   ],
   imports: [
     CommonModule,
