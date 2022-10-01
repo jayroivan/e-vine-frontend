0fade0489b092f541a6a093ddc954ec3a64d316e
dani3lm3na1999 <dani3lm3na@gmail.com>
2022-10-01 13:32:34 -0600

    enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL:'https://e-vine.herokuapp.com',
+  firebaseConfig: {
+    apiKey: "AIzaSyD7lQs6wJJ8OH71iL3cqY2D9hAVvcFQ3uM",
+    authDomain: "e-vine.firebaseapp.com",
+    projectId: "e-vine",
+    storageBucket: "e-vine.appspot.com",
+    messagingSenderId: "60640068410",
+    appId: "1:60640068410:web:523b05c6a1064ef9308603",
+    measurementId: "G-KWQMPSWLTR"
+  }
 };
